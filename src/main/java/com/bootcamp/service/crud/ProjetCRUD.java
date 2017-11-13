729401cccddf51ca46fc422b307bee3be1ed3ecd
fbr <bignonfebron@gmail.com>
2017-11-13 16:06:21 +0100

    adding of userCrud

--- src/main/java/com/bootcamp/service/crud/ProjetCRUD.java
+++ src/main/java/com/bootcamp/service/crud/ProjetCRUD.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 /**
  *
- * @author edwigegédéon
+ * @author Bignon
  */
 public class ProjetCRUD
 {
--- src/main/java/com/bootcamp/service/crud/UserCRUD.java
+++ src/main/java/com/bootcamp/service/crud/UserCRUD.java
@@ -2,42 +2,49 @@
 package com.bootcamp.service.crud;
 
 import static com.bootcamp.constants.AppConstants.PERSISTENCE_UNIT;
-import com.bootcamp.entities.Projet;
-import com.bootcamp.jpa.ProjetRepository;
+import com.bootcamp.entities.User;
+import com.bootcamp.jpa.UserRepository;
 import java.sql.SQLException;
 import java.util.List;
 
 /**
  *
- * @author edwigegédéon
+ * @author Bignon
  */
-public class ProjetCRUD
+public interface UserCRUD
 {
-    public static void create(Projet projet) throws SQLException{
-        ProjetRepository pr = new ProjetRepository(PERSISTENCE_UNIT);
-        pr.create(projet);
+    public static void create(User user) throws SQLException{
+        UserRepository pr = new UserRepository(PERSISTENCE_UNIT);
+        pr.create(user);
     }
     
-    public static void update(Projet projet) throws SQLException{
-        ProjetRepository pr = new ProjetRepository(PERSISTENCE_UNIT);
-        pr.update(projet);
+    public static void update(User user) throws SQLException{
+        UserRepository pr = new UserRepository(PERSISTENCE_UNIT);
+        pr.update(user);
     }
     
-    public static void delete(Projet projet) throws SQLException{
-        ProjetRepository pr = new ProjetRepository(PERSISTENCE_UNIT);
-        pr.delete(projet);
+    public static void delete(User user) throws SQLException{
+        UserRepository pr = new UserRepository(PERSISTENCE_UNIT);
+        pr.delete(user);
     }
     
-    public static List<Projet> findAll() throws SQLException{
-        ProjetRepository pr = new ProjetRepository(PERSISTENCE_UNIT);
-        List<Projet> liste =pr.findAll();
+    public static List<User> findAll() throws SQLException{
+        UserRepository pr = new UserRepository(PERSISTENCE_UNIT);
+        List<User> liste =pr.findAll();
         return liste;
     }
     
-    public static List<Projet> findByProperty(String propertyName, Object value) throws SQLException{
-        ProjetRepository pr = new ProjetRepository(PERSISTENCE_UNIT);
-        List<Projet> projets = pr.findByProperty(propertyName, value);
+    public static List<User> findByProperty(String propertyName, Object value) throws SQLException{
+        UserRepository pr = new UserRepository(PERSISTENCE_UNIT);
+        List<User> users = pr.findByProperty(propertyName, value);
         
-        return projets;
+        return users;
+    }
+    
+    public static User findByPropertyUnique(String propertyName, Object value) throws SQLException{
+        UserRepository pr = new UserRepository(PERSISTENCE_UNIT);
+       User user = pr.findByPropertyUnique(propertyName, value);
+        
+        return user;
     }
 }
